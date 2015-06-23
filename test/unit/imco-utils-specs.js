7df468cc29f4d9e93153d06d247f0024d4d3fe7b
hugomosh <hugomosh@gmail.com>
2015-06-23 12:50:49 -0500

    Mejoras a utils

--- test/unit/imco-utils-specs.js
+++ test/unit/imco-utils-specs.js
@@ -5,22 +5,52 @@ describe('imco', function() {
     beforeEach(inject(function(_utils_) {
         utils = _utils_;
     }));
-    it('Deberia ser capitalizar azul', function() {
+    it('Deberia capitalizar azul', function() {
         var color = 'azul';
         expect(color.capitalize()).toBe('Azul');
     });
-    /*;
 
-        it('should test the homePages controller', inject(function($controller, $rootScope) {
-            var ctrl = $controller('HomeCtrl', {
-                $scope: $rootScope
-            });
-            expect($rootScope.welcome_message.length).toBeGreaterThan(0);
-        }));
-       
-              it('should properly provide a welcome message', inject(function(welcomeMessage) {
-                  expect(welcomeMessage()).toMatch(/welcome/i);
-              }));
-          */
+    it('Deberia quitar acentos de  "Me gusta la comprobación -> "Me gusta la comprobacion"', function() {
+        var color = 'Me gusta la comprobación';
+        expect(color.removeDiacritics()).toBe('Me gusta la comprobacion');
+    });
+    it('Deberia convertir "Me gusta la comprobación" a url "me_gusta_la_comprobacion"', function() {
+        var color = 'Me gusta la comprobación';
+        expect(color.toURL()).toBe('me_gusta_la_comprobacion');
+    });
+    it('Debería imprimir en consola ', function() {
+
+        console.log = jasmine.createSpy("log");
+        var imprimeIMCO = utils.imprimeIMCO("blue");
+        expect(console.log).toHaveBeenCalledWith('%c                                                                                                    \n' +
+            '                                                             .                                      \n' +
+            '                                                            ::                                      \n' +
+            '                                                           ,7                                       \n' +
+            '                                                          .7                                        \n' +
+            '                                                          $,                                        \n' +
+            '                                                          I                                         \n' +
+            '                                                         :?::,                 I$$$$$=              \n' +
+            '     :+7Z$$Z?  ,+$Z$$$O                Z$$Z        ,IZZZ$777$$$$ZZ$         77,       :7+           \n' +
+            '        ,$$$+      77$$?              7$$$$      ?$$=    ~I    :Z7$      ~$,        .    :Z         \n' +
+            '         $7$+      I77$$+             $?7$$    =$7$      :7,   : =$     I          ,+      ?+       \n' +
+            '         $7$+      I$ $$Z:           $7 77$    $$$        7=   :. 7    ?=          ~+       ++      \n' +
+            '         $7$+      I$  $7Z          I$  7$$   I7$7        77:   I :   +=           $         ?      \n' +
+            '         $7$+      I$  ?$$?        ?$?  7$$   $$$~        ,77   ?7.   $           $~     ,=   ?     \n' +
+            '         $7$+      I$   $$$:       $$   7$$  :$$$:         +77   77   I         ,7+     .7    $     \n' +
+            '         $7$+      I$    $$Z:     ?Z    7$$  :$7$~          ,77?  :77++       +7$=     :+     $     \n' +
+            '         $7$+      I$     $7Z    +Z,    7$$   $$$I           ,77$+  ~I777$77777,     +$=      ?     \n' +
+            '         $7$+      I$     $$$:   $$     77$   777$             ?777+  Z,~++=.      +77       :.     \n' +
+            '         $7$+      I$      $$$: $$      77$   ,77$,              ?77I7I7?~:::~+7$77I         $      \n' +
+            '         $$$+      I$       $$$+Z,      7$$    I$7$~                ~?$7777777$I=,         ~7       \n' +
+            '         $7$+      I$       :$$7+       77$     ?$$$             ~Z    ,Z$,               ,+        \n' +
+            '         $$7$,     I$I       7$$        77$=      7$7Z7:,  ,:?$Z$+       $$Z~           II          \n' +
+            '         ZZZZZZZ:  7ZZZZ$     Z+        $ZZZOZI     ?$Z$$$$ZZ$?,          ~Z7$$+:, ,:I$$$           \n' +
+            '                                                                             +$$$$$7HM$$I:          \n' +
+            '                                                                                                    \n' +
+            '                                                                                                    \n', 'color:' + "blue" + ';'
+        );
+
+    });
+
 
 });
